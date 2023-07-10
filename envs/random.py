42764613d865bd3192489c95f8fe58e1196fa944
Guy <camillarhodes@yandex.com>
2023-07-10 13:15:31 +0200

    more efficient impl. of uniform allocation

--- envs/random.py
+++ envs/random.py
@@ -5,7 +5,7 @@ from typing import Tuple
 
 
 def random_mdp(
-        n_states: int, n_actions: int, horizon: int, non_reachable_states: int = 0, zero_action: int = 1
+        horizon:int, n_states: int, n_actions: int, non_reachable_states: int = 0, zero_action: int = 1
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
 
     # Transition model
--- irl/algo.py
+++ irl/algo.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tqdm
+import itertools
 
 from typing import Tuple
 
@@ -9,56 +10,74 @@ def get_design_matrix(horizon: int, n_states: int, n_actions: int, zero_action: 
     X = get_X_matrix(horizon, n_states, n_actions, zero_action)
     return X.T@X
 
-def get_X_matrix(horizon: int, n_states: int, n_actions: int, zero_action: int = 1) -> np.ndarray:
+def get_X(horizon: int, n_states: int, n_actions: int, zero_action: int = 1) -> np.ndarray:
+
+    states_eye = np.eye(n_states)
+    horizon_eye = np.eye(horizon)
+    vec = np.zeros(n_actions)
+    vec[0] = 1
+    X_zero_actions =  np.kron(horizon_eye, np.kron(states_eye, vec))
+
 
     n = 1
-    mat = np.zeros((n, n_actions))
-    if zero_action:
-        mat[0,0] = 1
+    action_mat = np.zeros((n, n_actions))
     for action_plus in range(n_actions):
         for action_minus in range(action_plus+1, n_actions):
             vec = np.zeros(n_actions)
             vec[action_plus] = 1
             vec[action_minus] = -1
-            mat2 = np.zeros((n+1, n_actions))
-            mat2[:-1,:] = mat
-            mat2[-1,:] = vec
-            mat = mat2
+            action_mat2 = np.zeros((n+1, n_actions))
+            action_mat2[:-1,:] = action_mat
+            action_mat2[-1,:] = vec
+            action_mat = action_mat2
             n += 1
-    states_eye = np.eye(n_states)
-    horizon_eye = np.eye(horizon)
-    return np.kron(horizon_eye, np.kron(states_eye, mat))
+    action_mat = action_mat[1:,:]
 
-def get_action_preferences(X: np.ndarray, reward: np.ndarray, horizon: int, n_states: int, n_actions: int, zero_action: int = 1) -> np.ndarray:
+    X =  np.kron(horizon_eye, np.kron(states_eye, action_mat))
+    return X, X_zero_actions
+
+def get_preference_probabilities(X: np.ndarray, reward: np.ndarray, horizon: int, n_states: int, n_actions: int, zero_action: int = 1, fast=True) -> np.ndarray:
 
     assert (np.max(reward) <= 0.5) and (np.min(reward) >= -0.5)
-    Y = np.zeros(X.shape[0])
-    Q = np.zeros(X.shape[0])
+    Y = None
     for h in range(horizon):
         for s in range(n_states):
-            for action_plus in range(n_actions):
-                for action_minus in range(action_plus+1, n_actions):
-                    index_in_X = int((h*n_states+s)*(zero_action+n_actions*(n_actions-1)/2) \
-                            + action_plus*(2*n_actions - action_plus-1)/2 + action_minus + zero_action - action_plus) -1
-                    vec = np.zeros(X.shape[1])
-                    vec[(h*n_states+s)*n_actions +  action_plus] = 1
-                    vec[(h*n_states+s)*n_actions +  action_minus] = -1
-                    if not all(X[index_in_X] == vec):
-                        import ipdb; ipdb.set_trace()
-                    p = 0.5*(reward[h, s, action_plus] - reward[h, s, action_minus] + 1)
-                    Y[index_in_X] =  2*np.random.binomial(1, p, 1)-1
+            reward_action_pairs = np.array(list(itertools.combinations(reward[h, s, :], 2)))
+            if h==0 and s==0:
+                prob = 0.5*(reward_action_pairs[:,0]-reward_action_pairs[:,1] + 1)
+                    #Y = 2*np.random.binomial(1, p)-1
+            else:
+                    #Y = np.concatenate([Y, 2*np.random.binomial(1, p)-1])
+                prob = np.concatenate([prob, 0.5*(reward_action_pairs[:,0]-reward_action_pairs[:,1] + 1)])
                     #Q[index_in_X] =  reward[h, s, action_plus] - reward[h, s, action_minus]
-    return Y
+    return prob
 
-def perform_uniform_allocation(X: np.ndarray, reward: np.ndarray, horizon: int,  n_states: int, n_actions: int, zero_action: int = 1, n_rounds: int=1, verbose=True) -> np.ndarray:
-    Y = None
-    it = tqdm.tqdm(range(n_rounds)) if verbose else range(n_rounds)
+def perform_uniform_allocation(X: np.ndarray, X_zero_actions: np.ndarray, reward: np.ndarray, horizon: int,  n_states: int, n_actions: int, zero_action: int = 1, eps: float=0.01, delta: float=0.05, verbose=True) -> np.ndarray:
+
+    Y = np.zeros(X.shape[0])
+    step = len(Y)
+    n_rounds = int(1+8*n_states*n_actions*horizon**3*np.log(2/delta) / eps**2)
+    if step < n_rounds:
+        n_rounds += step - n_rounds % step
+    if verbose:
+        print(f'Performing uniform allocation with eps: {eps:.4f}, delta: {delta:.2f}')
+        print(f'Number of rounds: {n_rounds}')
+
+
+    prob = get_preference_probabilities(X, reward, horizon, n_states, n_actions, zero_action)
+
+    #prob = np.tile(prob, step)
+    rng = range(0, n_rounds, step)
+    it = tqdm.tqdm(rng) if verbose else range(rng)
     for r in it:
-        Y_round = get_action_preferences(X, reward, horizon, n_states, n_actions, zero_action)
-        if Y is None:
-            Y = Y_round
-        else:
-            Y = np.concatenate([Y, Y_round])
-    return np.tile(X, (n_rounds, 1)), Y
+        Y_round = 2*np.random.binomial(1, prob)-1
+        Y += Y_round
+    Y /= (n_rounds/step)
+
+    zero_actions = np.zeros(horizon*n_states)
+    Y = np.concatenate([zero_actions, Y])
+    X_combined = np.concatenate([X_zero_actions, X], axis=0)
+    r_hat = np.linalg.inv(X_combined.T@X_combined)@X_combined.T@Y
+    return r_hat
 
 
